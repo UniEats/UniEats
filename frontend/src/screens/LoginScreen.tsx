5bf2d3159cff7851f5b8417da07c4308a30cacd3
Ulises Tripaldi <utripaldi@fi.uba.ar>
2025-11-05 21:06:22 +0000

    Update file LoginScreen.tsx

--- frontend/src/screens/LoginScreen.tsx
+++ frontend/src/screens/LoginScreen.tsx
@@ -1,10 +1,7 @@
 import { CommonLayout } from "@/components/CommonLayout/CommonLayout";
 import { Login } from "@/components/Login/Login";
-import { normalizeRole, useToken } from "@/services/TokenContext";
 import { useLogin } from "@/services/UserServices";
-import { useLocation } from "wouter";
 import styles from "./LoginScreen.module.css";
-import { LoginRequest } from "@/models/Login";
 
 export const LoginScreen = () => {
   const { mutate, error, isPending } = useLogin();
